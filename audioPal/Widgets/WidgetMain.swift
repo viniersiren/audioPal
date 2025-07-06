bea30294f51078f8ec9f9e18a9cbb9dcb0f47c14
viniersiren <devinstuddard@gmail.com>
2025-07-05 18:39:07 -0700

    deleted old duplicate widget

--- audioPal/Widgets/WidgetMain.swift
+++
@@ -1,9 +0,0 @@
-import WidgetKit
-import SwiftUI
-
-// Widget extension entry point - no @main needed when in same project
-struct AudioPalWidgetExtension: WidgetBundle {
-    var body: some Widget {
-        AudioPalWidget()
-    }
-} 
